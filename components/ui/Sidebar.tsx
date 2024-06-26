406f5903db477af90ab2e668138533fab4373ade
Usmanwasif123 <usmanwasif8@gmail.com>
2024-06-26 23:07:33 +0500

    mobile side sidebar half make

--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -13,7 +13,7 @@ const Sidebar = ( {user}: SiderbarProps) => {
   return (
     <section className='sidebar'>
         <nav className='flex flex-col gap-4'>
-            <Link href='/' className='mb-12 cursor-pointer 
+            <Link href='/' className='mb-12 flex cursor-pointer 
             items-center gap-2'>
                 <Image src='/icons/logo.svg'
                 width={34}
@@ -25,7 +25,7 @@ const Sidebar = ( {user}: SiderbarProps) => {
             </Link>
 
             {sidebarLinks.map((item) => {
-              const isActive = item.route
+              const isActive = pathname === item.route
                || pathname.startsWith(`${item.route}/`)
 
               return (
@@ -33,11 +33,26 @@ const Sidebar = ( {user}: SiderbarProps) => {
                 className={cn('sidebar-link', {
                   'bg-bank-gradient': isActive})
                   }>
-                    {item.label}
+                    <div className='relative size-6'>
+                      <Image 
+                      src={item.imgURL}
+                      alt={item.label}
+                      fill
+                      className={cn({
+                         'brightness-[3] invert-0': isActive
+                      })}/>
+                    </div>
+                    <p className={cn('sidebar-label',{
+                      '!text-white': isActive })
+                      }>{item.label}
+                    </p>
                 </Link>
               )
             })}
+            USER
         </nav>
+
+            FOOTER
     </section>
   )
 }
